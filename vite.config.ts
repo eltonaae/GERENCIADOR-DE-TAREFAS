c1f58a0c3460bc424bcd595d7791234600a75848
eltonaae <elton.aae@gmail.com>
2026-08-19 22:08:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'serve' ? '/' : '/TOP-AniRanker/',
+    base: command === 'serve' ? '/' : '/GERENCIADOR-DE-TAREFAS/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
